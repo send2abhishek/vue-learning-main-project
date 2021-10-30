69a1ac11837ea4b5fd033323fb2fddb4f904286c
Abhishek Kumar <abhishek160.kumar@ril.com>
2021-10-30 07:31:07 +0530

    add coach requsest pages and the store elelments

--- src/pages/requests/RequestReceived.vue
+++ src/pages/requests/RequestReceived.vue
@@ -1,1 +1,51 @@
-<template>request</template>
+<template>
+  <section>
+    <base-card>
+      <header>
+        <h2>Request Received</h2>
+      </header>
+      <ul v-if="hasRequest">
+        <request-item
+          v-for="request in receivedRequest"
+          :key="request.id"
+          :email="request.userEmail"
+          :message="request.uerMessage"
+        ></request-item>
+      </ul>
+      <h3 v-else>You haven't received the request yet!</h3>
+    </base-card>
+  </section>
+</template>
+
+<script>
+import RequestItem from '@/components/requests/RequestItem.vue';
+
+export default {
+  components: { RequestItem },
+  computed: {
+    receivedRequest() {
+      return this.$store.getters['requests/requests'];
+    },
+    hasRequest() {
+      return this.$store.getters['requests/hasRequests'];
+    },
+  },
+};
+</script>
+
+<style scoped>
+header {
+  text-align: center;
+}
+
+ul {
+  list-style: none;
+  margin: 2rem auto;
+  padding: 0;
+  max-width: 30rem;
+}
+
+h3 {
+  text-align: center;
+}
+</style>
